9ef808e4bd36719ec40f80f2bb1ca5975a8d9653
saheen <saheenshoukath@gmail.com>
2020-09-27 21:10:31 +0530

    added simple layout

--- pages/index.js
+++ pages/index.js
@@ -5,60 +5,33 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>saheen.codes</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
+      <img src="/logoepic.png" alt="epiclogo" width="250"/>
+
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="#">saheen.codes</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Yet Another Coder
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div class={styles.socialIconList}>
+         <a href="https://www.facebook.com/BeastHaxor/" target="_blank"> <img src="https://img.icons8.com/color/48/000000/facebook.png" class={styles.socialIcon}/></a>
+         <a href="https://www.youtube.com/channel/UCO9nfwZE7J7GkubwmUna9tA" target="_blank"> <img src="https://img.icons8.com/fluent/48/000000/youtube-play.png" class={styles.socialIcon}/></a>
+         <a href="https://www.instagram.com/saheenshoukath/" target="_blank"> <img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" class={styles.socialIcon}/></a>
         </div>
-      </main>
+       
+        </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        <a href="https://www.instagram.com/saheenshoukath/"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+          Designed by SaheenShoukath</a>
       </footer>
     </div>
   )
