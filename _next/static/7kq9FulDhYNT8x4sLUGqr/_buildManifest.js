bd8f67c51d0f3e6b7e514c935c2263c50a4b4b38
shellord <saheen@relicstudio.dev>
2020-09-27 18:02:26 +0000

    Deploying to gh-pages from  @ 241966e0047f0eb0cf988f3010b8478515047fd8 🚀

---
+++ _next/static/7kq9FulDhYNT8x4sLUGqr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fe85d175d4e8857d64ee0.css","static\u002Fchunks\u002Fpages\u002Findex-7afbedeccf0fe8e27da6.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
